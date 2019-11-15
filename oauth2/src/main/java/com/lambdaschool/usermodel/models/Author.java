93faf9315ecf869921fe43e98c9cc840fb669e4c
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-15 10:01:17 -0700

    Update Author.java

--- oauth2/src/main/java/com/lambdaschool/usermodel/models/Author.java
+++ oauth2/src/main/java/com/lambdaschool/usermodel/models/Author.java
@@ -1,13 +1,16 @@
 package com.lambdaschool.usermodel.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.lambdaschool.usermodel.logging.Loggable;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Loggable
 @Entity
 @Table(name = "authors")
-public class Author {
+public class Author extends Auditable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,6 +18,16 @@ public class Author {
     private String fname;
     private String lname;
 
+    @OneToMany(mappedBy = "author",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("author")
+    private List<Wrote> wrote = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    @JsonIgnoreProperties("author")
+    private List<Book> books = new ArrayList<>();
 
     public Author() {}
 
