c74bc9b5a4a513ab7b74ee028f3f56be175fdd2d
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-15 10:20:46 -0700

    Create SectionService.java

---
+++ oauth2/src/main/java/com/lambdaschool/usermodel/services/SectionService.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.usermodel.services;
+
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.Section;
+
+import java.util.List;
+
+public interface SectionService
+{
+    List<Section> findAll();
+
+    Section findSectionById(long id);
+
+    void delete(long id);
+
+    Section save(Section section);
+
+    Section findByName(String name);
+
+    Section update(long id,
+                Section section);
+}
