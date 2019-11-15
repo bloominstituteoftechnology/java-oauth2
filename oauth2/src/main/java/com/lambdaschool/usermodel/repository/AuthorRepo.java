68a9696ba0b73558f9bcf01f2d98b5fc8bb07dcc
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-15 10:18:24 -0700

    other repos

---
+++ oauth2/src/main/java/com/lambdaschool/usermodel/repository/AuthorRepo.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.usermodel.repository;
+
+import com.lambdaschool.usermodel.models.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepo extends CrudRepository<Author, Long> {
+}
