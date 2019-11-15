8dc53839e6b1abc9c526bc1084e58c60caedd4dd
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-15 14:15:27 -0700

    Create BookControllerTest.java

---
+++ oauth2/src/test/java/com/lambdaschool/usermodel/controllers/BookControllerTest.java
@@ -0,0 +1,12 @@
+package com.lambdaschool.usermodel.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookControllerTest {
+
+    @Test
+    public void getBooks() {
+    }
+}
