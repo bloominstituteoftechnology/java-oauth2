41a87eae9523de2fed1a30eb613f2dd2f2a03eb1
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-15 09:35:46 -0700

    Update Section.java

--- oauth2/src/main/java/com/lambdaschool/usermodel/models/Section.java
+++ oauth2/src/main/java/com/lambdaschool/usermodel/models/Section.java
@@ -1,4 +1,36 @@
 package com.lambdaschool.usermodel.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sectionid;
+
+    private String name;
+
+
+    public Section(){}
+    public Section(String name) {
+        this.name = name;
+    }
+
+    public long getSectionid() {
+        return sectionid;
+    }
+
+    public void setSectionid(long sectionid) {
+        this.sectionid = sectionid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
