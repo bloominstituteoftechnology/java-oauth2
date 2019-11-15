ccbaa236b5b41c17640a6903b0a274ad5186884b
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-11-15 09:47:12 -0700

    Create Book.java

---
+++ oauth2/src/main/java/com/lambdaschool/usermodel/models/Book.java
@@ -0,0 +1,61 @@
+package com.lambdaschool.usermodel.models;
+
+import com.lambdaschool.usermodel.logging.Loggable;
+
+import javax.persistence.*;
+
+@Loggable
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long bookid;
+
+    private String title;
+    private String ISBN;
+    @Column(nullable = true)
+    private int copy;
+
+
+    public Book(){}
+
+    public Book(String title, String ISBN, int copy) {
+        this.title = title;
+        this.ISBN = ISBN;
+        this.copy = copy;
+    }
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
+}
