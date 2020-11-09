0e0bbaeb12c6c2bfd121303cda5d5f0b75a4fbc9
John Mitchell <jrmmba@outlook.com>
2020-11-09 12:18:37 -0800

    Updating Documentation

--- usermodel-initial/src/main/java/com/lambdaschool/usermodel/services/HelperFunctionsImpl.java
+++ usermodel-initial/src/main/java/com/lambdaschool/usermodel/services/HelperFunctionsImpl.java
@@ -1,10 +1,12 @@
 package com.lambdaschool.usermodel.services;
 
 import com.lambdaschool.usermodel.models.ValidationError;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +20,10 @@ public class HelperFunctionsImpl
         // data validations get wrapped in other exceptions as we work through the Spring
         // exception chain. Hence we have to search the entire Spring Exception Stack
         // to see if we have any violation constraints.
-        while ((cause != null) && !(cause instanceof ConstraintViolationException))
+        while ((cause != null) && !(cause instanceof org.hibernate.exception.ConstraintViolationException || cause instanceof MethodArgumentNotValidException))
         {
+            System.out.println(cause.getClass()
+                .toString());
             cause = cause.getCause();
         }
 
@@ -28,14 +32,34 @@ public class HelperFunctionsImpl
         // we know that cause either null or an instance of ConstraintViolationException
         if (cause != null)
         {
-            ConstraintViolationException ex = (ConstraintViolationException) cause;
-            for (ConstraintViolation cv : ex.getConstraintViolations())
+            if (cause instanceof org.hibernate.exception.ConstraintViolationException)
             {
+                org.hibernate.exception.ConstraintViolationException ex = (ConstraintViolationException) cause;
                 ValidationError newVe = new ValidationError();
-                newVe.setCode(cv.getInvalidValue()
-                                      .toString());
-                newVe.setMessage(cv.getMessage());
+                newVe.setCode(ex.getMessage());
+                newVe.setMessage(ex.getConstraintName());
                 listVE.add(newVe);
+            } else
+            {
+                if (cause instanceof MethodArgumentNotValidException)
+                {
+                    MethodArgumentNotValidException ex = (MethodArgumentNotValidException) cause;
+                    List<FieldError> fieldErrors = ex.getBindingResult()
+                        .getFieldErrors();
+                    for (FieldError err : fieldErrors)
+                    {
+                        ValidationError newVe = new ValidationError();
+                        newVe.setCode(err.getField());
+                        newVe.setMessage(err.getDefaultMessage());
+                        listVE.add(newVe);
+                    }
+                } else
+                {
+                    System.out.println("Error in producing constraint violations exceptions. " +
+                        "If we see this in the console a major logic error has occurred in the " +
+                        "helperfunction.getConstraintViolation method that we should investigate. " +
+                        "Note the application will keep running as this only affects exception reporting!");
+                }
             }
         }
         return listVE;
--- usermodel/src/main/java/com/lambdaschool/usermodel/controllers/OpenController.java
+++ usermodel/src/main/java/com/lambdaschool/usermodel/controllers/OpenController.java
@@ -94,7 +94,7 @@ public class OpenController
         // return the access token
         // To get the access token, surf to the endpoint /login just as if a client had done this.
         RestTemplate restTemplate = new RestTemplate();
-        String requestURI = "http://" + httpServletRequest.getServerName() + ":" + httpServletRequest.getLocalPort() + "/login";
+        String requestURI = "http://localhost" + ":" + httpServletRequest.getLocalPort() + "/login";
 
         List<MediaType> acceptableMediaTypes = new ArrayList<>();
         acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
